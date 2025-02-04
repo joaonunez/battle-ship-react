695c23dccd10131d33729c5cdb6ed565ee65d927
joaonunez <joaovaldiglesias@gmail.com>
2025-02-03 23:32:28 -0300

    mejora de comentarios en mi proyecto para entenderlo mejor

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 
+//funcion para generar nuestro tablero con posiciones aleatorias 
 const generateRandomBoard = () =>{
-    const rows = 10;
-    const cols = 10;
+    const rows = 10; //declaramos numero de filas
+
+    const cols = 10; //declaramos numero de columnas
+
     const board = Array(rows) //creamos un Array de 10 elementos undefined
+
     .fill(null) // para evitar problemas con map en el console log ala hora de testear convertimos a null para poder ver la matriz en la consola
     //console.log(board) = [null, null, null, null, null, null, null, null, null, null]
-    .map(() => Array(cols).fill(0));
+
+    .map(() => Array(cols).fill(0)); //esto recorera cara elemento del array que tenemos de 10 null, y los convertira a 10 arrays de 10 ceros, logrando que se convierta en una matriz
+
     // console.log(board)  obtenemos una matriz de 10x10 llena de ceros
 
 }
