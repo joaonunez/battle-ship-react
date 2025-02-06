528b62d07c0543f55b09b9ec4b2df95755c749af
joaonunez <joaovaldiglesias@gmail.com>
2025-02-06 13:55:40 -0300

    instalacion de framer motion para animacion de renderizado en App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,80 @@
-import { useState } from 'react';
-import { setTurn } from './utils/gameLogic';
-import GameBoard from './components/boards/GameBoard';
-import TurnIndicator from './components/interactive-messages/TurnIndicator';
+import { useState } from "react";
+import { setTurn } from "./utils/gameLogic";
+import GameBoard from "./components/boards/GameBoard";
+import TurnIndicator from "./components/interactive-messages/TurnIndicator";
+import { AnimatePresence, motion } from "framer-motion";
 
 function App() {
   //declaramos que el turno del jugador sera un estado cambiante
-  const[isPlayerTurn,setPlayerTurn] = useState(true);
+  const [isPlayerTurn, setPlayerTurn] = useState(true);
 
   //funcionalidad para cambiar el estado del turno
   const handleTurnChange = () => {
     //decidi agregar el intervalo de tiempo para que se vea dinamico y no tan computarizado
-    setTimeout(()=>{
+    setTimeout(() => {
       setPlayerTurn(setTurn(isPlayerTurn));
-    },1000)
+    }, 1000);
+  };
+
+  //animaciones de renderizado para la aplicacion
+  const pageVariants = {
+    initial: { opacity: 0, y: -50 },
+    animate: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: 50 },
+  };
+
+  const pageTransition = {
+    duration: 0.5,
+    ease: "easeInOut",
   };
 
   return (
     <>
-      <div className='app'>
-        <h1 className='text-center mt-3'>Batalla De Embarcaciones</h1>
-        
-        
-        {/* cambie true a isPlayerTurn COMO ARGUMENTO, para que lea el estado del turno */}
-        <TurnIndicator isPlayerTurn={isPlayerTurn}  /> 
+    <div className="app">
+        <AnimatePresence>
+        <motion.div variants={pageVariants}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={pageTransition}>
+          
+        <h1 className="text-center mt-3">Batalla De Embarcaciones</h1>
+        </motion.div>
 
-        {/* //al tener onTurnChange como prop es posible llamarlo como funcion en el componente GameBoard */}
-        <GameBoard isPlayerTurn={isPlayerTurn} onTurnChange={handleTurnChange}  isAITurn={!isPlayerTurn} />
+        </AnimatePresence>
+        
+        
+
+        <AnimatePresence mode="wait">
+          {/* Animación de entrada/salida para el indicador de turno */}
+          <motion.div
+            variants={pageVariants}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={pageTransition}
+          >
+            <TurnIndicator isPlayerTurn={isPlayerTurn} />
+          </motion.div>
+        </AnimatePresence>
+
+        <AnimatePresence mode="wait">
+          {/* Animación para el tablero de juego, dependiendo de si es turno del jugador o la IA */}
+          <motion.div
+            variants={pageVariants}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={pageTransition}
+          >
+            <GameBoard
+              isPlayerTurn={isPlayerTurn}
+              onTurnChange={handleTurnChange}
+              isAITurn={!isPlayerTurn}
+            />
+          </motion.div>
+        </AnimatePresence>
       </div>
-
     </>
   );
 }
